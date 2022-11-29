af62695d3db04d86d0fead9c8cfd807ee4b1136b
iykethe1st <iykethe1st@gmail.com>
2022-11-29 17:50:28 +0000

    00...99

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for num in range(0, 100):
+    if num == 99:
+        print("{}".format(num))
+    elif num < 99:
+        if num < 10:
+            print("0{}".format(num), end=", ")
+        else:
+            print("{}".format(num), end=", ")
+    
