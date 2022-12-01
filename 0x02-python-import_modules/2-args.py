b81908980c539ffbe7ed1ff57a807a7585ccc891
iykethe1st <iykethe1st@gmail.com>
2022-12-01 17:45:22 +0000

    Task 2 commit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    total_arg = len(sys.argv)
+    if total_arg <= 1:
+        print("0 arguments")
+    else:
+        if total_arg == 2:
+            print("{} argument".format(total_arg - 1))
+        else:
+            print("{} arguments".format(total_arg - 1))
+        for i in range(1, total_arg):
+            print("{}: {}".format(i, sys.argv[i]))
