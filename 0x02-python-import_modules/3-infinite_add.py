aae63ada8b9c379225a4a98d7367b0e5c2d5d1af
iykethe1st <iykethe1st@gmail.com>
2022-12-01 18:01:39 +0000

    Task 3 commit

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys    
+    total_arg = len(sys.argv)
+    total = 0
+    for i in range(1, total_arg):
+        total += int(sys.argv[i])
+    print("{}".format(total))
