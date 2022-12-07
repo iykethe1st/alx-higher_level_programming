bd8242b50da45c2d25cede92fa67949c02e4b5c2
iykethe1st <iykethe1st@gmail.com>
2022-12-07 22:01:05 +0000

    hot fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,45 +1,10 @@
 #!/usr/bin/python3
-def get_value(x):
-    if (x == 'I'):
-        return 1
-    if (x == 'V'):
-        return 5
-    if (x == 'X'):
-        return 10
-    if (x == 'L'):
-        return 50
-    if (x == 'C'):
-        return 100
-    if (x == 'D'):
-        return 500
-    if (x == 'M'):
-        return 1000
-    return -1
-
 def roman_to_int(roman_string):
-    sum = 0
-    i = 0
-    
-    if not (isinstance(roman_string, str)) or roman_string == None:
+    if not roman_string or type(roman_string) != str:
         return 0
-    
-    if (roman_string):
-    
-        while (i < len(roman_string)):
-            num1 = get_value(roman_string[i])
-        
-            if (i + 1 < len(roman_string)):
-                num2 = get_value(roman_string[i + 1])
-        
-                if (num1 >= num2):
-                    sum += num1
-                    i += 1
-        
-                else:
-                    sum += num2 - num1
-                    i += 2
-            else:
-                sum += num1
-                i += 1
-    
-    return sum
+    total = 0
+    digits = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    for roman in reversed(roman_string):
+        arabic = digits[roman]
+        total += arabic if total < arabic * 5 else -arabic
+    return total
